ae95acc05b0702700e3e3a26246ad96c416471be
Rcbonetto <34357322+Rcbonetto@users.noreply.github.com>
2019-05-08 22:50:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+// item uno  - alternamos el cambio de color del título principal
+function colorCambio(selector) {
+	$(selector).animate({
+			opacity: '1',
+		}, {
+			step: function () {
+				$(this).css('color', 'white');
+			},
+			queue: true
+		})
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'red');
+			},
+			queue: true
+		}, 600)
+		.delay(1000)
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'white');
+			},
+			queue: true
+		})
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'red');
+				colorCambio('h1.main-titulo');
+			},
+			queue: true
+		});
+}
+
+// iniciamos el juego
+function initializeGame() {
+
+	colorCambio('h1.main-titulo');
+
+	$('.btn-reinicio').click(function () {
+		if ($(this).text() === 'Reiniciar') {
+			location.reload(true);
+		}
+		checkBoard();
+		$(this).text('Reiniciar');
+		$('#timer').startTimer({
+			onComplete: endGame
+		})
+	});
+}
+
+// Preparamos el juego
+$(function() {
+	initializeGame();
+});
